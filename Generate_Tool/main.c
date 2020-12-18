52127ef5f8bd65a230e098b351717270633adae9
Cory Hodge <coryhodge@Corys-MBP.attlocal.net>
2020-12-17 21:44:35 -0500

    copy pasted working file parser to repo main.c, needs includes updated and will not build, but need to save work at this point

--- Generate_Tool/main.c
+++ Generate_Tool/main.c
@@ -26,104 +26,202 @@
 #include <stdlib.h>
 #include <string.h> // for string length()
 
-// == GEN TOOL RESOURCES =======================================================
+// == GEN TOOL RESOURCES =====================
 
 //! \brief Project Definitions
 #define MAX_COL_PER_LINE    81
+#define ZERO                0
+#define TAG_BEGIN_INDEX     8
 
 //! \brief Input Buffer
 char BUFFER[MAX_COL_PER_LINE];
 
-//! \brief Create file pointers
+//! \brief Index for parsing string char by char
+int BUFFER_INDEX;
+
+//! \brief Charcter Arrays for holding important
+//!          info parsed from file
+
+//! \brief  TAG is entire string 
+//!           including MOD , REG, BF
+const int MAX_TAG_SIZE = 20;
+char TAG_FULL_NAME[MAX_TAG_SIZE];
+
+//! \brief  Module Name
+const int MAX_MODULE_NAME_SIZE = 20;
+char MODULE_NAME[MAX_MODULE_NAME_SIZE];
+
+//! \brief Register Name
+const int MAX_REG_NAME_SIZE = 20;
+char REGISTER_NAME[MAX_REG_NAME_SIZE];
+
+//! \brief Bit Field Name
+const int MAX_BF_NAME_SIZE = 20;
+char BF_NAME[MAX_BF_NAME_SIZE];
+
+//! \brief Address as a STRING
+const int MAX_MM_VALUE_SIZE = 10;
+char MM_VALUE[MAX_MM_VALUE_SIZE];
+
+//! \brief File pointers
+//! \brief Input Memory Map FP
 FILE * in_MemoryMap_fp;
+
+//! \brief Output Register File FP
 FILE * out_Structure_fp;
 
+// FUNCTIONS
+void ASSERT_FP(FILE * fp){
+    if(fp == NULL){
+        // DEBUG
+        printf("file pointer null\n");
+        printf("ERROR: Return 1\n");  
+        exit;
+    }
+}
+
+//! \brief Increment index of Buffer through empty spaces
+void skip_spaces(char * array, int index){
+    while(array[index] == ' '){
+        
+        // DEBUG
+        //printf("skipping spaces");
+        index++;
+    }
+}
+
+/**
+ * 
+ *      DEBUGGING
+ * 
+ */
+int lines_skipped = 0;
 
 
 
-// Index for parsing string char by char
-int position;
 
-// ************* MAIN **********************************************************
+// ************* MAIN *************************
 int main(void) {
 
-    // open file to parse
-    in_MemoryMap_fp = fopen("../MemMaps/hw_sysctl.h", "r");
+    // open file to parse read only, it must exist
+    in_MemoryMap_fp = fopen("hw_sysctl_copy.h", "r");
+    ASSERT_FP(in_MemoryMap_fp);
 
-    // open file to write truncate file if it already exists
-    out_Structure_fp = fopen("../FrameWork/test_Struct.txt", "w+");
+    // open file to write, truncate file if it already exists
+    out_Structure_fp = fopen("test_Struct.h", "w+");
+    ASSERT_FP(out_Structure_fp);
 
-    // until end of file reached, 
-    // Copy line from file up to 'MAX_COL_PER_LINE' characters
+    // DEBUG
+    //fprintf(out_Structure_fp, "Writing to new File");
+
+    /**
+     * Parse File line by line until EOF reached
+     */
     while( fgets(BUFFER, MAX_COL_PER_LINE, in_MemoryMap_fp) != NULL ){
-
-        /*    Parse String and pull out needed info for building structs    */
-
+        
         // Reset Index to 0
-        position = 0;
+        BUFFER_INDEX = 0;
+        
+        // skep spaces up to useable char 
+        skip_spaces(BUFFER, BUFFER_INDEX);
 
+        /**
+         * Character at BUFFER_INDEX : /
+         * Start of Comment
+         */
+        // DEBUG
+        //printf("continuing?\n");
+        if ( BUFFER[BUFFER_INDEX] == '/' ) {
+            //printf("BUFFER[0] == '/'\n");
+            // TODO: implement comment parsing
 
-        /*
-        // if : first character of string is //
-        // then this line is a comment
-        */
-        if ( BUFFER[0] == '/' ) {
-
-            // TODO : implement comment parsing
+            // continue back to while loop, get next line
+            lines_skipped++;
             continue;
         }
 
-        /*
-        // if : first character of string is #
-        // then this line is an address or mask
-        */
-        else if ( BUFFER[position] == '#') {
+        /**
+         * Character at BUFFER_INDEX : #
+         * #define 
+         */
+        if ( BUFFER[BUFFER_INDEX] == '#') {
+            
+            //printf("lines skipped %d \n", lines_skipped);
 
-            // TODO: move these to constants area or define
-            int INDEX_MODULE_NAME_BEGIN = 8;
-            int MAX_MODULE_NAME_SIZE = 40;
-            char FULL_NAME_STRING[MAX_MODULE_NAME_SIZE];
-            int full_name_index = 0;
-            /**     * *    Mask         * *
-            *  I need 6 pieces of information:
-            *  Entire_Combo_Name
-            *  Module
-            *  Register
-            *  Bit_Field ( optional )
-            *  Address/Mask
-            *  Comment String
-            */
+            // DEBUG
+            //printf("BUFFER[0] == # \n");
 
-            // Parse Full Defined Name
-            position = INDEX_MODULE_NAME_BEGIN;
+            // skip #define 
+            BUFFER_INDEX = TAG_BEGIN_INDEX;
+                 
+            // DEBUG
+            //printf("BUFFER[TAG_BEGIN_INDEX] == 8 \n");
+/////////////////////////////////////////////////////////////////////////////
+            // copy whole tag up to first space
+            int tag_index = 0;
+            while(BUFFER[BUFFER_INDEX] != ' '){
 
-            // until a space is reached
-            while(BUFFER[position] != ' '){
+                // DEBUG
+                //printf("Buffer[index] == %c ", BUFFER[BUFFER_INDEX]);
 
-                // copy characters of FULL_NAME_STRING
-                FULL_NAME_STRING[full_name_index] = BUFFER[position];
-
-                // increment index's
-                position++;
-                full_name_index++;
+                TAG_FULL_NAME[tag_index] = BUFFER[BUFFER_INDEX];
+                tag_index++;
+                BUFFER_INDEX++;
             }
+        }
 
-            // proceed to skip spaces until '/' continuation key or 0x address begin is found
+        // skep spaces up to useable char 
+        skip_spaces(BUFFER, BUFFER_INDEX);
+        
+        /**
+         * Character at BUFFER_INDEX : 0
+         * Start of Address
+         */
+        if(BUFFER[BUFFER_INDEX] == '0'){
+           // copy 32 bit address or mask up too space
+            int value_index = 0;
+            while(BUFFER[BUFFER_INDEX] != ' '){
+                MM_VALUE[value_index] = BUFFER[BUFFER_INDEX];
+                BUFFER_INDEX++;
+                value_index++;
+            }
+        }
 
+        /**
+         * Character at BUFFER_INDEX : \
+         * Adress of current buffer on next line
+         */
+        if(BUFFER[BUFFER_INDEX] == '\\'){
+                
+        }
+
+        /**
+         * Character at BUFFER_INDEX : ' '
+         * Empty character
+         */
+        if ( BUFFER[BUFFER_INDEX] == ' ') {
+            //printf("BUFFER[0] == space \n");  
 
 
         }
 
         /**
-         * Line does not begin with # or / 
+         * Character at BUFFER_INDEX : unknown
          */
         else {
 
+            printf("BUFFER[BUFFER_INDEX] == ??? \n");
+            //printf("%c \n", BUFFER[BUFFER_INDEX]);
+            continue;
         }
 
 
+        printf("\n ACQUIRED VALUES \n\n");
+        printf("Full Tag Name : %s \n", TAG_FULL_NAME);
+        printf("VALUE : %s \n", MM_VALUE);
 
     } // End File Parse
 
+    printf("EXIT: Return 0\n");
     return 0;
 } // End Main
