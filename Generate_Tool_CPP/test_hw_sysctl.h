a92d7cbe53a8735e2116beaa185e8a8b8ffe3dbc
Cory Hodge <coryhodge@Corys-MBP.attlocal.net>
2020-12-23 09:05:09 -0500

    updating template header module

---
+++ Generate_Tool_CPP/test_hw_sysctl.h
@@ -0,0 +1,55 @@
+//****************/
+//
+//  Modules
+//
+//*****************
+
+#define module1_BASE_ADDRESS 0x00000001     // Module 1 Base Address
+#define module2_BASE_ADDRESS 0x00000010     // Module 2 Base Address
+
+//****
+//  Mod 1 Reg's's
+//*****
+
+#define module1_register1   0x0000000x  // module 1 reg 1
+#define module1_register2    0x000000x0  // module 1 reg 2
+
+//****
+//  Mod 2 Reg's's
+//***
+
+#define module2_register1   0x0000000x  // module 2 reg 1
+#define module2_register2    0x000000x0  // module 2 reg 2
+
+
+//****
+//  Mod 1 Reg 1 BF's
+//*****
+
+#define module1_register1_m1_r1_bf1 0x0000000b  // mod 1 reg 1 bf 1
+#define module1_register1_m1_r1_bf2 0x000000b0  // mod 1 reg 1 bf 2
+#define module1_register1_m1_r1_bf3 0x000000bb  // mod 1 reg 1 bf 3
+
+//****
+//  Mod 1 Reg 2 BF's
+//*****
+
+#define module1_register2_m1_r2_bf1 0x0000000b  // mod 1 reg 2 bf 1
+#define module1_register2_m1_r2_bf2 0x000000b0  // mod 1 reg 2 bf 2
+#define module1_register2_m1_r2_bf3 0x000000bb  // mod 1 reg 2 bf 3
+
+//****
+//  Mod 2 Reg 1 BF's
+//*****
+
+#define module2_register1_m2_r1_bf1 0x0000000b  // mod 2 reg 1 bf 1
+#define module2_register1_m2_r1_bf2 0x000000b0  // mod 2 reg 1 bf 2
+#define module2_register1_m2_r1_bf3 0x000000bb  // mod 2 reg 1 bf 3
+
+//****
+//  Mod 2 Reg 2 BF's
+//*****
+
+#define module2_register2_m2_r2_bf1 0x0000000b  // mod 2 reg 2 bf 1
+#define module2_register2_m2_r2_bf2 0x000000b0  // mod 2 reg 2 bf 2
+#define module2_register2_m2_r2_bf3 0x000000bb  // mod 2 reg 2 bf 3
