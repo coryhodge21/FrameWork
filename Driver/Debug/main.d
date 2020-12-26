6947bdbef6df110eaa3c873f0f8d4c49926c7447
Cory Hodge <coryhodge@Corys-MBP.attlocal.net>
2020-12-26 18:03:56 -0500

    FrameWork_V1 complete. Driver Program includes my generated FrameWork/ library, and uses the structures to set actual bits in hardware registers. Tested and works!!!!!!!!!!!!! Fuck ya!!!

--- Driver/Debug/main.d
+++ Driver/Debug/main.d
@@ -1,23 +1,9 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: ../MOD_REG.h
-main.obj: /Users/coryhodge/Documents/Engineering/Embedded/FrameWork/Driver/MOD_REG_enums.h
-main.obj: ../prj_common.h
-main.obj: /Applications/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdlib.h
-main.obj: /Applications/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
-main.obj: /Applications/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
-main.obj: /Applications/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
-main.obj: ../Output.h
-main.obj: ../Output_enums.h
+main.obj: /Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2.h
+main.obj: /Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2_enums.h
 
 ../main.c: 
-../MOD_REG.h: 
-/Users/coryhodge/Documents/Engineering/Embedded/FrameWork/Driver/MOD_REG_enums.h: 
-../prj_common.h: 
-/Applications/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdlib.h: 
-/Applications/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h: 
-/Applications/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h: 
-/Applications/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h: 
-../Output.h: 
-../Output_enums.h: 
+/Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2.h: 
+/Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2_enums.h: 
