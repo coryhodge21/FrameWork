0d251020aa124ff54da2810ad9e6b5a10c955d37
Cory Hodge <coryhodge@Corys-MBP.attlocal.net>
2020-12-25 22:50:27 -0500

    new GenTool Project. i will add each file into the new project space as it seems to be working and I cant debug the current workspace after the update

--- GenTool/main.cpp
+++ GenTool/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
-#include <filesystem>
 
 int main() {
-
-    std::filesystem::create_directory("../FrameWork/newDir");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
