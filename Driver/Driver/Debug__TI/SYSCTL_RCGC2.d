df3374a20b64fe1dcef43c8c80c299b5153ceacb
Cory Hodge <coryhodge@Corys-MBP.attlocal.net>
2020-12-27 22:51:03 -0500

    rm gen files

--- Driver/Driver/Debug__TI/SYSCTL_RCGC2.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-SYSCTL_RCGC2.obj: /Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2.c
-SYSCTL_RCGC2.obj: /Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2.h
-SYSCTL_RCGC2.obj: /Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2_enums.h
-
-/Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2.c: 
-/Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2.h: 
-/Users/coryhodge/Documents/Engineering/Embedded/FrameWork/FrameWork/SYSCTL/Registers/RCGC2/SYSCTL_RCGC2_enums.h: 
